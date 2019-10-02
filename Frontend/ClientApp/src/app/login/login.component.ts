d4bdd0b5e66f91e5f2eac6f3fc7aa2b525da98ee
Stefan Vissers <vissestefan2@hotmail.nl>
2019-10-02 10:24:16 +0200

    Login Forms added

--- Frontend/ClientApp/src/app/login/login.component.ts
+++ Frontend/ClientApp/src/app/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+    model: any = {};
 
-  ngOnInit() {
-  }
+    errorMessage: string;
+    constructor(private router: Router, private LoginService: LoginService) { }
 
-}
+
+    ngOnInit() {
+        sessionStorage.removeItem('UserName');
+        sessionStorage.clear();
+    }
+    login() {
+        debugger;
+        this.LoginService.Login(this.model).subscribe(
+            data => {
+                debugger;
+                if (data.Status == "Success") {
+                    this.router.navigate(['/Dashboard']);
+                    debugger;
+                }
+                else {
+                    this.errorMessage = data.Message;
+                }
+            },
+            error => {
+                this.errorMessage = error.message;
+            });
+    };
+} 
